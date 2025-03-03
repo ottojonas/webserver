91ffda166daf40fb94782daf79aef7213b34cabb
ottojonas <ottobjonas@outlook.com>
2025-03-03 14:12:25 +0000

    removed comments

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
     "net/http"
 )
 
-// helloHandler responds with a simple message.
 func helloHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "wagwan")
 }
@@ -16,14 +15,9 @@ func usersHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-    // Set up the route for "/"
     http.HandleFunc("/", helloHandler)
 	http.HandleFunc("/api/users", usersHandler)
-
-    // Define the port to listen on
     port := ":3001"
     fmt.Printf("Server is running on http://localhost%s\n", port)
-
-    // Start the server and log any errors.
     log.Fatal(http.ListenAndServe(port, nil))
 }
